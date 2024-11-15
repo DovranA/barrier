f26e5389cf3a5829bb74f3f4812b49713825678a
DovranA <atadov23@gmail.com>
2024-11-15 23:16:34 +0500

    for tech2

--- src/socket/socket.controller.ts
+++ src/socket/socket.controller.ts
@@ -1,15 +1,23 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { SocketGateway } from './socket.gateway';
+import { SocketService } from './socket.service';
+import { EnterCarDto } from 'src/car/dto/car-input.dto';
 
 @Controller('socket')
 export class SocketController {
-  constructor(private readonly socketGateway: SocketGateway) {}
+  constructor(
+    private readonly socketGateway: SocketGateway,
+    private socketService: SocketService,
+  ) {}
 
   @Post('open')
-  triggerEvents() {
+  async triggerEvents(@Body() dto: EnterCarDto) {
+    const transaction = await this.socketService.enterCar(dto);
+    if (transaction) {
+      this.socketGateway.server.emit('openBarrier', { open: true });
+      this.socketGateway.server.emit('refreshAdmin', { refresh: true });
+    }
     // Emit events when the HTTP POST request is received
-    this.socketGateway.server.emit('openBarrier', { open: true });
-    this.socketGateway.server.emit('refreshAdmin', { refresh: true });
     return { message: 'Events triggered successfully' };
   }
 }
